8c0037e84c677a84217bcfe1df3a5ba6cee34916
matthieumada <ben97.com@gmail.com>
2025-12-08 13:55:14 +0100

    Start jumping
    STrat Jumping but need to chane the stifnees of the virtual model

--- Simulation/main.py
+++ Simulation/main.py
@@ -5,18 +5,26 @@ import time
 import queue
 import matplotlib.pyplot as plt
 from controller import impedance_control
+#from no_mistake import impedance_control 
 PI = np.pi
 
 # --- Main function ---
 def simulate_free_motion(model_path, sim_time):
+
+    # data
+    Joint = []
+    Torque_data= []
+    Force = []
+    ground_contact = []
+    position = []
+    ref_z = [] 
+    length = []
     # load model 
     m = mujoco.MjModel.from_xml_path(model_path)
     d = mujoco.MjData(m)
-    # goal 
-    pos_des = np.array([-0.05, 0.7])
     
     # Initial paramter for only one control 
-    d.qpos[:] = np.array([-0.3,-PI/4, PI/4]) # position:[hip (m) , upper(rd), leg(rd)]
+    d.qpos[:] = np.array([0,-PI/4, PI/2]) # position:[hip(m) , upper(rd), leg(rd)]
     d.qvel[:] = np.zeros(m.nv)  # speed 
 
     # update phyics computation (kinematics, dynamics, sensors, contacts without move th eismulation 
@@ -38,25 +46,23 @@ def simulate_free_motion(model_path, sim_time):
 
         for i in range(steps):
             # forces data 
-            #print(sensor_id) 
-            hip_pos = d.sensordata[hip_sensor_id]
+            hip_pos =float(d.sensordata[hip_sensor_id])
+            print("Hip position=", hip_pos)
+
             adr = m.sensor_adr[sensor_id]       # Id of froce sensor in sensor_data
             dim = m.sensor_dim[sensor_id]       # Dimension of data 
             foot_force = d.sensordata[adr : adr + dim]
             f_norm = np.sqrt(foot_force[0]*foot_force[0] + foot_force[1]*foot_force[1] + foot_force[2]*foot_force[2])
-            torque = impedance_control(model=m, d=d, pos_des=pos_des, f=f_norm, torque=torque)
-            #print("Force sol sur pied (N) :",foot_force)
-            #print("Norm force", f_norm 
-            
-            # Free system
-            #d.ctrl[:] = 0.0
-            # controller
-            #pos = impedance_control(model=m, data=d, pos_des=pos_des, f=f_norm)
-            # pos_computed.append(pos)
-            # pos_desired.append(pos_des[1])
-            # pos_measured.append(hip_pos)
-            d.ctrl[:] = torque 
+            torque, pos, pos_des, L, Ld = impedance_control(d= d, x_hip=-0.16, z_hip=hip_pos, f_ground=f_norm)
+            Force.append(f_norm)
+            length.append(L)
+            Torque_data.append(torque)
+            ground_contact.append(foot_force)
+            Joint.append(d.qpos[[1,2]])
+            ref_z.append(hip_pos)
+            position.append(pos)
             # Move to a new state of the simulation 
+            d.ctrl = torque
             mujoco.mj_step(m, d) 
 
             # synchronization viewer
@@ -68,14 +74,88 @@ def simulate_free_motion(model_path, sim_time):
 
             # Option : real time system
             time.sleep(timestep)
-    print(m.actuator(0).name)
-    print(m.actuator(1).name)
+        print("Simulation finished. Viewer stays open. Close the window to exit.")
+        while viewer.is_running():
+            viewer.sync()
+            time.sleep(0.01)
 
+    plt.subplot(121)
+    plt.title("Leg Torque")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Torque [Nm]")
+    plt.plot(T, np.array(Torque_data)[:,1], label="Leg joinnt")
+    plt.legend()
+    plt.subplot(122)
+    plt.title("Upper leg Torque")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Torque [Nm]")
+    plt.plot(T, np.array(Torque_data)[:,0], label="Uppee joint")
+    plt.legend()
+    plt.savefig("./display/Torque")
 
+    plt.figure()
+    plt.title("Force measured by sensor over time")
+    plt.xlabel("Time [s]")
+    plt.ylabel("F [N]")
+    plt.plot(T, np.array(Force), label="F_ground")
+    plt.legend()
+    plt.savefig("./display/Force")
+
+    plt.figure()
+    plt.title("Joint positions over time")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Angle [rad]")
+    plt.plot(T, np.array(Joint)[:,0], label="Upper joint")
+    plt.plot(T, np.array(Joint)[:,1], label="Leg joint")
+    plt.legend()
+    plt.savefig("./display/Joint")
+
+    position = np.array(position)
+    length_des = Ld * np.ones(np.shape(position)[0])
+    des_x = pos_des[0] * np.ones(np.shape(position)[0]) 
+    des_z = pos_des[1] * np.ones(np.shape(position)[0]) 
+    ref_z = np.array(ref_z)
+    plt.figure()
+    plt.subplot(121)
+    plt.title("Position on  X axis")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Axis x [m]")
+    plt.plot(T, position[:,0], color='b' ,label="Leg" )
+    plt.plot(T, des_x, color='r', label="x goal")
+    plt.legend()
+    
+
+    plt.subplot(122)
+    plt.title("Position on Z axis")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Axis z [m]")
+    plt.plot(T, position[:,1], color='b', label="Leg")
+    plt.plot(T, ref_z, color='k', label="hips")
+    plt.plot(T, des_z , color='r', label="leg_ref")
+    plt.legend()
+    plt.savefig("./display/Position")
+
+    plt.figure()
+    plt.title("Length in stance phase")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Length [m]")
+    plt.plot(T, length_des, label="L_des")
+    plt.plot(T, np.array(length), label="L")
+    plt.legend()
+    plt.savefig("./display/Length")
+    
+    print("Model=", model_path)
+    print("Timestep =",timestep, "steps =",steps)
+    print("Sensor list :", [m.sensor(i).name for i in range(m.nsensor)])
+    print("Actuators:", m.nu)
+    for i in range(m.nu):
+        name = mujoco.mj_id2name(m, mujoco.mjtObj.mjOBJ_ACTUATOR, i)
+        print("id=",i,"name_actuator=",name)
+    print("force without contact", Force[-1])
+    print("x desired=", des_x[-1], "real_x=",position[:,1][-1], "difference=", des_x[-1] - position[:,1][-1])
     return 
-
 
 # --- Main launcher  ---
 if __name__ == "__main__":
-    model_path = "stand_complete.xml"  # XML file
+    model_path = "ref_stand.xml"  # XML file
     simulate_free_motion(model_path, sim_time=3.0)
--- Simulation/no_mistake.py
+++ Simulation/no_mistake.py
@@ -6,30 +6,31 @@ PI = np.pi
 L1 = 0.16 # m
 L2 = 0.16 # m
 m_leg = 15*10**(-3) # kg
-ks = 10000 # N/m
-kd = 50# Ns/m
+ks = 750000 # N/m
+kd = 5000 # Ns/m
 
 # Controller parameter: stiffness and damping
-kx = 5.0 * 10 **(2)# N/m
-kz = 5.0 * 10 **(2) # N/m
-mx = 3.0 * 10 **(1) # Ns/m 
-mz = 3.0 * 10 **(1) # Ns/m 
+kx = 1.0 * 10 ** 4 # N/m
+kz = 1.0 * 10 ** 4 # N/m
+K = np.array([kx, kz])
+mx = 0 #100 # 2.5 # Ns/m 
+mz = 0#100 #2.5 # Ns/m 
+M = np.array([mx, mz])
 
 def forward_kinematics(q, dq,  x_hip, z_hip):
-    q0 = float(q[0])#-3*PI/4 # fixed 
-    q1 = float(q[1])
+    q0 = q[0]#-3*PI/4 # fixed 
+    q1 = q[1]
 
+    # take th eposiiton of the hip from the model
+    pos_foot = np.zeros((2,1)) #[x,z]
     delta_x = L1*np.sin(q0) + L2*np.sin(q0 + q1)
     delta_z = L1*np.cos(q0) + L2*np.cos(q0 + q1)
-
-    # take the posiiton of the hip from the model
-    pos_foot = np.zeros((2,1)) #[x,z]
-    pos_foot[0] = x_hip - delta_x #x
-    pos_foot[1] = z_hip - delta_z #z 
+    pos_foot[0] = x_hip + delta_x #x
+    pos_foot[1] = z_hip + delta_z #z 
 
     # Jacobian 
-    J = np.array([[-L1*np.cos(q0) - L2*np.cos(q0+q1), -L2*np.cos(q0+q1)],
-                  [ L1*np.sin(q0)  + L2*np.sin(q0+q1),  L2*np.sin(q0+q1)]])
+    J = np.array([[L1*np.cos(q0) + L2*np.cos(q0+q1), L2*np.cos(q0+q1)],
+                  [-L1*np.sin(q0) - L2*np.sin(q0+q1),  -L2*np.sin(q0+q1)]])
     
     # lenght of the vritual model 
     L = np.sqrt( (delta_x)**2 + (delta_z)**2 )
@@ -37,33 +38,29 @@ def forward_kinematics(q, dq,  x_hip, z_hip):
     return  pos_foot, J, L, L_point, delta_x, delta_z
 
 def impedance_control(d, x_hip, z_hip, f_ground):
-    Ld = 0.2 # desired leg length with all leg have a angle of pi/4 
-    pos_des = np.array([ x_hip, z_hip - 0.2])
+    Ld = 0.25 # desired leg length with all leg have a angle of pi/4 
+    pos_des = np.array([ x_hip, z_hip])
 
     # Joint state
     q = d.qpos[[1, 2]]
     dq = d.qvel[[1, 2]]
     # Forward kinematics (position of foot)
     pos_foot, J, L, L_point, delta_x, delta_z = forward_kinematics(q, dq, x_hip, z_hip) 
-    if f_ground > 23.15:
+
+    if f_ground > 50:
         print("Ground reaction force due to contact =", f_ground,"--> Stance phase")
         # virtual force control 
-        print("L=",L)
         F_leg = - ks*(L - Ld ) - kd*L_point
         F = F_leg * np.array([ delta_x/L, delta_z/L])  # convert to cartesian force
         torque =  J.T @ F # joint torques
         print("Torque in stance leg =", torque)
+        return torque, pos_foot
     else:
         print("No contact with the ground --> Swing phase")
         # PD position control 
-        F_swing = np.array((2,1))
-
-        # computation by hand
-        F_swing[0] = - kx*(pos_foot[0] - pos_des[0]) - mx * (J[0,0] *dq[0] - J[0,1] *dq[1])
-        F_swing[1] = - kz*(pos_foot[1] - pos_des[1]) - mz * (J[1,0] *dq[0] - J[1,1] *dq[1])
-
+        F_swing = - K @ (pos_foot - pos_des) -  M @ J[0, :]
         torque = J.T @ F_swing
         print("Torque in swing leg =", torque, "position_real=", pos_foot, "position_des=", pos_des)
-    return torque, pos_foot, pos_des, L, Ld
+        return torque, pos_foot
     
     
